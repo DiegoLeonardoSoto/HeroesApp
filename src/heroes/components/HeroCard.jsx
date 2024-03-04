ba038fdae2055d90a16b3934874210728ca00d17
Diego Soto <88800678+DiegoLeonardoSoto@users.noreply.github.com>
2024-03-04 11:47:13 -0300

    add HeroCard

---
+++ src/heroes/components/HeroCard.jsx
@@ -0,0 +1,49 @@
+import { Link } from 'react-router-dom'
+
+// eslint-disable-next-line react/prop-types
+const CharactersByHero = ({ alter_ego, characters }) => {
+  if (alter_ego === characters) return <></>
+
+  return <p>{characters}</p>
+}
+
+/* eslint-disable react/prop-types */
+const HeroCard = ({
+  id,
+  superhero,
+  publisher,
+  alter_ego,
+  first_appearance,
+  characters
+}) => {
+  const heroImageURL = `/assets/heroes/${id}.jpg`
+
+  return (
+    <div className="col">
+      <div className="card">
+        <div className="row no-gutters">
+          <div className="col-4">
+            <img src={heroImageURL} className="card-img" alt={superhero} />
+          </div>
+
+          <div className="col-8">
+            <div className="card-body">
+              <h5 className="card-title">{superhero}</h5>
+              <p className="card-text"> {alter_ego} </p>
+
+              <CharactersByHero characters={characters} alter_ego={alter_ego} />
+
+              <p className="card-text">
+                <small className="text-muted">{first_appearance}</small>
+              </p>
+
+              <Link to={`/hero/${id}`}>Mas...</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default HeroCard
--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -1,14 +1,15 @@
 import { getHeroesByPublisher } from '../helpers/getHeroesByPublisher'
+import HeroCard from './HeroCard'
 
 // eslint-disable-next-line react/prop-types
 export const HeroList = ({ publisher }) => {
   const heroes = getHeroesByPublisher(publisher)
 
   return (
-    <ul>
+    <div className="row - row-cols-1 row-cols-md-3 g-3">
       {heroes.map((heroe) => {
-        return <li key={crypto.randomUUID()}> {heroe.superhero} </li>
+        return <HeroCard key={heroe.id} {...heroe} />
       })}
-    </ul>
+    </div>
   )
 }
