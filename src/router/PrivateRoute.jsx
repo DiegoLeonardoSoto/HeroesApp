5462769f9a00063278a74061f3ebed2886849d15
Diego Soto <88800678+DiegoLeonardoSoto@users.noreply.github.com>
2024-03-05 10:54:58 -0300

    add PrivateRoute

---
+++ src/router/PrivateRoute.jsx
@@ -0,0 +1,10 @@
+import { useContext } from 'react'
+import { AuthContext } from '../auth/context/AuthContext'
+import { Navigate } from 'react-router-dom'
+
+// eslint-disable-next-line react/prop-types
+export const PrivateRoute = ({ children }) => {
+  const { logged } = useContext(AuthContext)
+
+  return logged ? children : <Navigate to={'/login'} />
+}
